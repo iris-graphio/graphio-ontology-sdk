7a5477b1b69440fdb44a0c2ec3177d6eff929e31
eunmiGo <148742489+eunmiGo@users.noreply.github.com>
2026-02-20 18:32:18 +0900

    [IG-3400] 버전 변경 및 구조 변경 - data_pipline 디렉토리 추가

--- graphio_sdk/__init__.py
+++ graphio_sdk/__init__.py
@@ -22,7 +22,7 @@ from graphio_sdk.ontology.object_type import ObjectTypeBase
 from graphio_sdk.ontology.query import ObjectSetQuery
 from graphio_sdk.ontology.edits import EditableObject, OntologyEditsBuilder
 from graphio_sdk.ontology.ontology import OntologyNamespace
-from graphio_sdk.meta_type import MetaTypeNamespace
+from graphio_sdk.data_pipline.meta_type import MetaTypeNamespace
 
 __all__ = [
     "GraphioClient",
